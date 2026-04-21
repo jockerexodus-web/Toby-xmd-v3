ceb5cebdd535d98a458c04ad1a0aa079b1da561f
jockerexodus-web <jockerexodus@gmail.com>
2026-04-21 11:37:59 +0100

    Create Config.js

---
+++ Config.js
@@ -0,0 +1,13 @@
+export default {
+  BotName: "*toby xmd v3*", //Bot name 
+  nameCreator: "toby dev", // your name 
+  Newsletter: "120363401981326656@newsletter",//newsletter for your channel
+  Newsletter2: " newsletter from your channel ",//optimel
+  Newsletter3: " Newsletter for your channel", //optimel
+
+Newsletter4: "Newsletter for your channel", //optimel
+
+Newsletter5: "Newsletter for your channel", //optimel
+
+Newsletter6: "Newsletter for your channel",//optimel 
+};
